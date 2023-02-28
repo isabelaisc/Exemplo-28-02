83e23ac247f1e59540dabc4798aef61a0bee9802
isabelaisc <isabelaisc2005@gmail.com>
2023-02-28 09:57:04 -0300

    first commit

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+</head>
+<body>
+        <h1>PÁGINA TESTE ABERTA!!!</h1>
+</body>
+</html>
